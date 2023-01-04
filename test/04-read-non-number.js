06ba86a6a53a70df6bb5e1c0c49c0d2b3e5700dc
vipul jain <vipul5798@github.com>
2023-01-04 14:27:17 +0530

    update test and package-lock

--- test/04-read-non-number.js
+++ test/04-read-non-number.js
@@ -8,8 +8,8 @@ var sf = __dirname + '/test.seq'
 test('reads non number sequence ids froim file', function(t) {
   var s = new SeqFile(sf)
   s.read(function(err,data){
-    t.equal(data, '1-1111')
-    t.equal(s.seq, '1-1111')
+    t.equal(data, '2-1111')
+    t.equal(s.seq, '2-1111')
     t.end()
   })
 })
